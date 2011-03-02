2bac66507b42f7bd4f5f58bf86f9a14f9418c001
Paperboy <paperboy@paperboy-wallpapers.com>
2011-03-02 03:47:09 +0100

    Cleaned up code

--- js/bag-of-tricks.js
+++ js/bag-of-tricks.js
@@ -1,3 +1,7 @@
+if (typeof window.console === 'undefined'){
+ var console = { log: function(e){/* alert(e) */} }
+}
+
 /**
  * This js includes:
  *
@@ -56,27 +60,37 @@ jQuery.cookie = function(name, value, options) {
  }
 };
 
-(function(moreModels, menuToggle, l, path){
- var moreModels = $('#model-list').find('.more-models'),
-     menuToggle = $('#menu-toggle'),
-     l = $.cookie('l'),
-     path = {path:'/'};
- menuToggle.click(function(){
-  if (moreModels.is(':hidden')){
-   moreModels.slideDown(200);
-   $.cookie('l', 'e', path);
-   return false;
-  } else {
-   moreModels.slideUp(200);
-   $.cookie('l', null, path); // changed from $.cookie('l','',{path:'/'});
-   return false;
-  }
- });
- if (l === 'e') moreModels.show();
+// Expandable model list, remember state by setting cookie
+(function() {
+    var moreModels = $('#model-list').find('.more-models'),
+        menuToggle = $('#menu-toggle'),
+        l = $.cookie('l'),
+        path = { path: '/' };
+    menuToggle.click(function() {
+        if (moreModels.is(':hidden')) {
+            moreModels.slideDown(200);
+            $.cookie('l', 'e', path);
+            return false;
+        } else {
+            moreModels.slideUp(200);
+            $.cookie('l', null, path);
+            return false;
+        }
+    });
+    if (l === 'e') { moreModels.show(); }
 })();
 
+// Top friend links tracking
+$('#toplist').find('li a').click(function(){ // prepend tys url to generated (inc/toplist.php) top friend links
+    $(this).attr('href','http://www.trackyourstats.com/cgibin/tys-listout.php?acc=1002396&site='+($(this).attr('href')));
+});
+
+// Other random stuff..
+$('#share').find('a').attr('target','_blank');
+$('#contact input:eq(2)').focus();
+
 $('a').click(function(){
- $(this).blur();
+    $(this).blur();
 });
 
 // Tracking of outbound link clicks
@@ -84,40 +98,74 @@ $('a').click(function(){
  // _gaq.push(['_trackEvent', 'External', 'Clicked', ($(this).text())+' - '+($(this).attr('href'))]);
 // });
 
-(function(){
+// Fn to calculate greatest common divisor -- GCD
+function gcdCalc(a, b) {
+    return (b === 0) ? a : gcdCalc(b, a % b);
+}
 
- var topList = $('#toplist').find('li a'),
-     wallpapers = $('#wallpapers'),
-     wallpaperLinks = wallpapers.find('dd a'),
-     screenSize = screen.width+'x'+screen.height,
-     matchedSize = {'style' : 'font-weight:bold', 'title' : 'This is your current screen resolution'},
-     scaledSize = {'style' : 'font-weight:bold', 'title' : 'This size should scale down nicely on your screen'};
+// Hints the user which wallpaper links ~match their current screen resolution
+(function() {
+    // set up user generated vars
+    var width = 1280, // hard coded for now to test res other than mine
+        height = 720,
+        gcd = gcdCalc(width, height),
+        aspect = (width / gcd) + ':' + (height / gcd),
+        pixels = width * height,
+        screenSize = width + 'x' + height,
+        // hardcoded aspect ratios, pixels and sizes available in #wallpapers
+        sizes = {
+            '5:4': {
+                1310720: '1280x1024'
+            },
+            '4:3': {
+                786432: '1024x768',
+                1920000: '1600x1200'
+            },
+            '16:10': {
+                1024000: '1280x800',
+                1296000: '1440x900',
+                1764000: '1680x1050',
+                2304000: '1920x1200'
+            },
+            '16:9': {
+                1049088: '1366x768',
+                2073600: '1920x1080'
+            }
+        },
+        // get elements and stuff..
+        wallpapers = $('#wallpapers'),
+        wallpaperLinks = wallpapers.find('dd a'),
+        match;
 
- topList.click(function(){ // prepend tys url to generated (inc/toplist.php) top friend links
-  $(this).attr('href','http://www.trackyourstats.com/cgibin/tys-listout.php?acc=1002396&site='+($(this).attr('href')));
- });
+    // not fully tested
+    wallpaperLinks.each(function() {
+        if (screenSize === $(this).text()) { // if users screen size matches anchor text, set attributes
+            $(this).addClass('match')
+                   .attr('title', 'This is your current screen resolution');
+        } else {
+            if (aspect in sizes) { // else check if users aspect ratio matches one of the objects in sizes
+                for (var prop in sizes[aspect]) { // then loop thru keys in matched object
+                    if (sizes[aspect].hasOwnProperty(prop) && prop > pixels) {
+                        match = sizes[aspect][prop]; // the first match is when key (pixels) is higher than users pixels
+                        break;
+                    }
+                }
+                if (match && match === $(this).text()) { // if match was found and a wallpapers that matches exist
+                    $(this).addClass('match')
+                           .attr('title', 'This size should scale down nicely on your screen'); // set secondary attributes
+                    //console.log(prop + " = " + sizes[aspect][prop]);
+                }
+            }
+        }
+    });
+    
+    wallpapers.delegate('a[href$=jpg]', 'click', function(){ // Tracking of wp downloads and set target _blank on wps
+        var url = $(this).attr('href');
+        $(this).attr('target','_blank');
+        //_gaq.push(['_trackEvent', 'Walls', 'Downloaded', url.substring(url.lastIndexOf('/')+1)]);
+    });
 
- $('#share').find('a').attr('target','_blank');
- $('#contact input:eq(2)').focus();
+    console.log(match); // 1920x1080
+    console.log(aspect);
 
- wallpapers.delegate('a[href$=jpg]', 'click', function(){ // Tracking of wp downloads and set target _blank
-  var url = $(this).attr('href');
-  $(this).attr('target','_blank');
-  _gaq.push(['_trackEvent', 'Walls', 'Downloaded', url.substring(url.lastIndexOf('/')+1)]);
- });
-
- wallpaperLinks.each(function(){
-  if (screenSize === $(this).text()){
-   $(this).attr(matchedSize);
-  } else if (screenSize === '1600x900'){
-   $('a:contains("1920x1080")').attr(scaledSize);
-  } else if (screenSize in{'1280x720':'', '854x480':''}){
-   $('a:contains("1366x768")').attr(scaledSize);
-  } else if (screenSize in{'1400x1050':'', '1280x960':'', '1152x864':''}){ // or: if (screenSize === ('1400x1050'||'1280x960'||'1152x864'))
-   $('a:contains("1600x1200")').attr(scaledSize);
-  } else if (screenSize === '800x600'){
-    $('a:contains("1024x768")').attr(scaledSize);
-  }
- });
- 
 })();
